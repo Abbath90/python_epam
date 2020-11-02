fd04e6de0302b18bdd853cb8ff67120549794f6e
Abbath90 <khal.timur@gmail.com>
2020-11-03 00:29:49 +0300

    task3_2

--- venv/homework1/task3/maxmin/maxmin.py
+++ venv/homework1/task3/maxmin/maxmin.py
@@ -1,13 +1,16 @@
-from typing import Tuple
+from typing import Tuple, List
 
-file_path = "some_file.txt"
 
-def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
+def read_file_to_list(file_name: str) -> List[int]:
     list_from_file = []
     with open(file_name) as fi:
         for line in fi:
             list_from_file.append(int(line))
+    return list_from_file
+
+
+def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
+    list_from_file = read_file_to_list(file_name)
     return (max(list_from_file), min(list_from_file))
 
-print(find_maximum_and_minimum(file_path))
 
--- venv/homework1/task3/tests/test_maxmin.py
+++ venv/homework1/task3/tests/test_maxmin.py
@@ -1,18 +1,18 @@
+from typing import List
 import pytest
 
-from calculator.calc import check_power_of_2
+from maxmin.maxmin import find_maximum_and_minimum
 
 
 @pytest.mark.parametrize(
     ["value", "expected_result"],
     [
-        (65536, True),
-        (12, False),
-        (0, False),
-        (1, True)
+        ("some_file_1.txt", [4, -1]),
+        ("some_file_1.txt", [4, -1]),
+        ("some_file_1.txt", [4, -1]),
     ],
 )
-def test_power_of_2(value: int, expected_result: bool):
-    actual_result = check_power_of_2(value)
+def test_maxmin(value: str, expected_result: List[int]):
+    actual_result = find_maximum_and_minimum(value)
 
     assert actual_result == expected_result
