770db0e05f4d435276e98f67d31180c9946a7f84
Abbath90 <khal.timur@gmail.com>
2020-11-06 21:30:41 +0300

    changes after PR

--- homework1/tests/test_maxmin.py
+++ homework1/tests/test_maxmin.py
@@ -4,16 +4,14 @@ from typing import Tuple
 import pytest
 from homework1.task3.maxmin import find_maximum_and_minimum
 
-path_to_test_stuff = "./homework1/tests/test_stuff/"
-
 
 @pytest.mark.parametrize(
     ["value", "expected_result"],
     [
-        (os.path.join(path_to_test_stuff, "test_file_maxmin_1.txt"), (100, -100)),
-        (os.path.join(path_to_test_stuff, "test_file_maxmin_2.txt"), (3, 1)),
-        (os.path.join(path_to_test_stuff, "test_file_maxmin_3.txt"), (2, 1)),
-        (os.path.join(path_to_test_stuff, "test_file_maxmin_4.txt"), (1, 1)),
+        ("./homework1/tests/test_stuff/test_file_maxmin_1.txt", (100, -100)),
+        ("./homework1/tests/test_stuff/test_file_maxmin_2.txt", (3, 1)),
+        ("./homework1/tests/test_stuff/test_file_maxmin_3.txt", (2, 1)),
+        ("./homework1/tests/test_stuff/test_file_maxmin_4.txt", (1, 1)),
     ],
 )
 def test_maxmin(value: str, expected_result: Tuple[int, int]):
@@ -22,7 +20,6 @@ def test_maxmin(value: str, expected_result: Tuple[int, int]):
     assert actual_result == expected_result
 
 
-# ./homework1/tests/test_stuff/test_file_maxmin_4.txt
 """@pytest.mark.parametrize(
     ["value", "expected_result"],
     [
