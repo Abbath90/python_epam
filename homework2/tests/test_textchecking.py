491f7ef95fd85b9a590039d61206b666d9205f4d
Abbath90 <khal.timur@gmail.com>
2020-11-09 18:47:55 +0300

    hw2 task1

---
+++ homework2/tests/test_textchecking.py
@@ -0,0 +1,24 @@
+from typing import List, Tuple, Dict
+
+import pytest
+
+from homework2.task1.text_checking import text_checking
+
+
+@pytest.mark.parametrize(
+    ["data", "expected_result"],
+    [
+        ("./homework2/tests/test_stuff/data.txt",
+         (['rückständig', 'schlagworte', 'schwerpunkt',
+          'moralischen', 'zukünftiges', 'landstriche',
+          'praktischen', 'erstaunlich', 'verständlich', 'kalyptischen'],
+          "›",
+          5391,
+          2972,
+          "ä")),
+    ],
+)
+def test_text_checking(data: str, expected_result: Tuple[List[str], str, int, int, str]):
+    actual_result = text_checking(data)
+
+    assert actual_result == expected_result
