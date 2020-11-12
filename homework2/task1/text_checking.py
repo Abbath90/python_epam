f8a0f8bb606262c372338db96ea39634ff4c8720
Abbath90 <khal.timur@gmail.com>
2020-11-12 13:24:14 +0300

    Revert "hw2 task1 changed"
    This reverts commit 7189d5ad

--- homework2/task1/text_checking.py
+++ homework2/task1/text_checking.py
@@ -1,85 +1,88 @@
+"""
+Given a file containing text. Complete using only default collections:
+    1) Find 10 longest words consisting from largest amount of unique symbols
+    2) Find rarest symbol for document
+    3) Count every punctuation char
+    4) Count every non ascii char
+    5) Find most common non ascii char for document
+"""
 from typing import Dict, List, Tuple
 
-import pytest
 
-from homework2.task1.text_checking import (
-    count_non_ascii_chars,
-    count_punctuation_chars,
-    get_longest_diverse_words,
-    get_most_common_non_ascii_char,
-    get_rarest_char,
-)
+def get_key_by_value(inp_dic: Dict, value: int) -> any:
+    for i in inp_dic.items():
+        if i[1] == value:
+            return i[0]
 
 
-@pytest.mark.parametrize(
-    ["data", "expected_result"],
-    [
-        (
-            "./homework2/tests/test_stuff/data.txt",
-            [
-                "rückständig",
-                "schlagworte",
-                "schwerpunkt",
-                "moralischen",
-                "zukünftiges",
-                "landstriche",
-                "praktischen",
-                "erstaunlich",
-                "verständlich",
-                "kalyptischen",
-            ],
-        ),
-    ],
-)
-def test_get_longest_diverse_words(data: str, expected_result: List[str]):
-    actual_result = get_longest_diverse_words(data)
+def text_checking(file_name: str) -> Tuple[List[str], str, int, int, str]:
+    chars_dict = {}
+    words_dict = {}
+    non_ascii_dict = {}
+    punctuation = """!?.,"'"<>«»:;{}[]()#$%^&*-+=/`~"""
+    ascii_num_set = list(range(128))
+    with open(file_name, encoding="unicode-escape", errors="replace") as fi:
+        for line in fi:
+            for word in line.lower().rstrip("\n").split(" "):
+                for char in word:
+                    if char in chars_dict:
+                        chars_dict[char] += 1
+                        if not ord(char) in ascii_num_set:
+                            non_ascii_dict[char] += 1
+                    else:
+                        chars_dict[char] = 1
+                        if not ord(char) in ascii_num_set:
+                            non_ascii_dict[char] = 1
+                translator = str.maketrans("", "", punctuation)
+                word = word.translate(translator)
+                if word in words_dict:
+                    words_dict[word] += 1
+                else:
+                    words_dict[word] = 1
 
-    assert actual_result == expected_result
+    longest_diverse_words = get_longest_diverse_words(words_dict)
+    rarest_char = get_rarest_char(chars_dict)
+    punctuation_chars = count_punctuation_chars(chars_dict)
+    non_ascii_chars = count_non_ascii_chars(non_ascii_dict)
+    most_common_non_ascii_char = get_most_common_non_ascii_char(non_ascii_dict)
 
-
-@pytest.mark.parametrize(
-    ["data", "expected_result"],
-    [
-        ("./homework2/tests/test_stuff/data.txt", "›"),
-    ],
-)
-def test_get_rarest_char(data: str, expected_result: str):
-    actual_result = get_rarest_char(data)
-
-    assert actual_result == expected_result
-
-
-@pytest.mark.parametrize(
-    ["data", "expected_result"],
-    [
-        ("./homework2/tests/test_stuff/data.txt", 5391),
-    ],
-)
-def test_count_punctuation_chars(data: str, expected_result: int):
-    actual_result = count_punctuation_chars(data)
-
-    assert actual_result == expected_result
+    return (
+        longest_diverse_words,
+        rarest_char,
+        punctuation_chars,
+        non_ascii_chars,
+        most_common_non_ascii_char,
+    )
 
 
-@pytest.mark.parametrize(
-    ["data", "expected_result"],
-    [
-        ("./homework2/tests/test_stuff/data.txt", 2972),
-    ],
-)
-def test_count_non_ascii_chars(data: str, expected_result: int):
-    actual_result = count_non_ascii_chars(data)
-
-    assert actual_result == expected_result
+def get_longest_diverse_words(words_dict: Dict[str, str]) -> List[str]:
+    output_list = []
+    for word in words_dict:
+        if len(list(word)) == len(set(word)):
+            output_list.append(word)
+    output_list.sort(key=len)
+    return output_list[-10:]
 
 
-@pytest.mark.parametrize(
-    ["data", "expected_result"],
-    [
-        ("./homework2/tests/test_stuff/data.txt", "ä"),
-    ],
-)
-def test_get_most_common_non_ascii_char(data: str, expected_result: str):
-    actual_result = get_most_common_non_ascii_char(data)
+def get_rarest_char(chars_dict: Dict[str, str]) -> str:
+    min_value_in_dict = min(chars_dict.values())
 
-    assert actual_result == expected_result
+    return get_key_by_value(chars_dict, min_value_in_dict)
+
+
+def count_punctuation_chars(chars_dict: Dict[str, str]) -> int:
+    punctuation = """!?.,"'"<>«»:;{}[]()#$%^&*-+=/`~"""
+    counter = 0
+    for i in chars_dict.keys():
+        if i in punctuation:
+            counter += chars_dict[i]
+    return counter
+
+
+def count_non_ascii_chars(non_ascii_dict: Dict[str, str]) -> int:
+    return sum((non_ascii_dict.values()))
+
+
+def get_most_common_non_ascii_char(non_ascii_dict: Dict[str, str]) -> str:
+    max_value_in_dict = max(non_ascii_dict.values())
+    return get_key_by_value(non_ascii_dict, max_value_in_dict)
