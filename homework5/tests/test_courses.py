118861fc7dd1423173429f54f27d20158d7f9c6a
Abbath90 <khal.timur@gmail.com>
2020-11-26 23:33:44 +0300

    hw5 tests fixed

--- homework5/tests/test_courses.py
+++ homework5/tests/test_courses.py
@@ -3,29 +3,21 @@ import pytest
 from homework5.task1.courses import Homework, Student, Teacher
 
 
-@pytest.mark.parametrize(
-    ["data", "expected_result"], [(Student("Roman", "Petrov"), True)]
-)
-def test_student_init(data: Student, expected_result: bool):
-    student = data
+def test_student_init():
+    student = Student("Roman", "Petrov")
 
     assert student.last_name == "Petrov"
 
 
-@pytest.mark.parametrize(
-    ["data", "expected_result"], [(Teacher("Daniil", "Shadrin"), True)]
-)
-def test_teacher_init(data: Teacher, expected_result: bool):
-    teacher = data
+def test_teacher_init():
+    teacher = Teacher("Daniil", "Shadrin")
 
     assert teacher.last_name == "Shadrin"
 
 
-@pytest.mark.parametrize(
-    ["data", "expected_result"], [(Homework("Learn functions", 0), True)]
-)
-def test_homework_init(data: Homework, expected_result: bool):
-    homework = data
+
+def test_homework_init():
+    homework = Homework("Learn functions", 0)
 
     assert homework.text == "Learn functions"
 
